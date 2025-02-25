9a7e82f4f5acc8041dc833282629fe9d4996cf27
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2025-02-25 17:07:17 +0200

    Migrate to standalone components and update bootstrapping

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { HighlightDirective } from 'src/app/shared/highlight.directive';
 
 @Component({
-  standalone: true,
-  imports: [SharedModule],
   selector: 'app-details',
+  standalone: true,
+  imports: [HighlightDirective],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
